9263520191bf7537f0009e3b990ff487f4cf64ce
Kristaps Bajazitovs <kristaps.bajazitovs@cube.lv>
2025-03-26 10:51:12 +0200

    Add config options to readme

--- config/filament-tree-view.php
+++ config/filament-tree-view.php
@@ -5,7 +5,5 @@ return [
 
     'has_permissions' => true,
 
-    // If set to true, will make the permissions system only check the user's permissions,
-    // this reduces permissions checks improving performance.
     'has_user_only_policy' => false,
 ];
